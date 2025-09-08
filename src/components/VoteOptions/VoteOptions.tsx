7ca0358b421e1626f0f8ca565461847851505169
Oksana Skakun <ksanas180882@gmail.com>
2025-09-08 20:20:51 +0300

    add all

--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -5,16 +5,19 @@ interface VoteOptionsProps {
     onVote: (type: VoteType) => void,
     onReset: () => void,
     canReset: boolean,
-    Reset: boolean,
 }
 
-export default function VoteOptions({onVote, onReset, canReset, Reset}: VoteOptionsProps) {
+export default function VoteOptions({ onVote, onReset, canReset }: VoteOptionsProps) {
     return (
         <div className={css.container}>
-            <button className={css.button} onClick={()=>onVote('good')}>Good</button>
-            <button className={css.button} onClick={()=>onVote('neutral')}>Neutral</button>
+            <button className={css.button} onClick={() => onVote('good')}>Good</button>
+            <button className={css.button} onClick={() => onVote('neutral')}>Neutral</button>
             <button className={css.button} onClick={() => onVote('bad')}>Bad</button>
-            {canReset && <button className={`${css.button} ${css.reset}`} onClick={onReset} disabled={!Reset}>Reset</button>}  
+            {canReset && (
+                <button className={`${css.button} ${css.reset}`} onClick={onReset}>
+                    Reset
+                </button>
+            )}
         </div>
-    ) 
-} 
+    );
+}
